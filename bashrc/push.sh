90508a4000e13329a8487bf8f2b15b502cfb9322
Simon Edstrom <simon.edstrom96@gmail.com>
2025-03-15 15:13:06 +0100

    feat: improve commit message generation with feedback loop

--- bashrc/push.sh
+++ bashrc/push.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Stages all files at current location, comes up with a good commit message based on conventional commits, commits it, pushes
-function push() {
-    git add .
 
+function generate_commit_message(){
+
+    feedback_message="$1"
+    feedback_prompt=""
+    if [[ -z "$rejected_commit_message" ]]; then
+        feedback_prompt="Additionally, the user has given you the following instructions/feedback: $feedback_message"
+    fi
+    
     commit_types="feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert"
 
-    SYSTEM_PROMPT="You exist in the users terminal as an assistant to create commit messages based on some file changes. Your output is to be the commit message AND NO OTHER TEXT. DO NOT include \`\`\`backticks\`\`\` or anything else, just the commit message. The commit message is to take the form of <type>:<content> where <type> is based on the conventional commit standard and <content> is the content of the commit message that summarises the changes made. You may choose from ONE of the following conventional commit types for the message: $commit_types. You may NOT use any other commit type. The message content should be short but perfectly summarise all changes made. As an example if the commit only contains that the README.md file was created, your full output should be: 'docs: created README file'. The content given from the user below contains a list of files and their git diffs in the commit and you are to base your response on that."
+    SYSTEM_PROMPT="You exist in the users terminal as an assistant to create commit messages based on some file changes. Your output is to be the commit message AND NO OTHER TEXT. DO NOT include \`\`\`backticks\`\`\` or anything else, just the commit message. The commit message is to take the form of <type>:<content> where <type> is based on the conventional commit standard and <content> is the content of the commit message that summarises the changes made. You may choose from ONE of the following conventional commit types for the message: $commit_types. You may NOT use any other commit type. The message content should be short but perfectly summarise all changes made. As an example if the commit only contains that the README.md file was created, your full output should be: 'docs: created README file'. The content given from the user below contains a list of files and their git diffs in the commit and you are to base your response on that. $feedback_prompt"
 
     SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     source $SCRIPT_DIR/call_llm.sh
@@ -34,14 +40,27 @@ function push() {
         CONTENT="$(call_llm "$SYSTEM_PROMPT_HEALING" "$CONTENT")"
     fi
 
-    echo $CONTENT
+    echo "$CONTENT"
+}
 
-    read -p "Above commit message ok? (press enter to accept): " input; if [ "$input" = "" ]; then
-        git commit -m "$CONTENT"
-        git push
-        return 0
-    else
-        return 0
-    fi
+function push() {
+    git add .
 
+    CONTENT="$(generate_commit_message "$*")"
+
+    echo "$CONTENT"
+
+    while true; do
+        read -r -p "Above commit message ok? (press enter to accept, write message for feedback): " input
+        if [ "$input" = "" ]; then
+            git commit -m "$CONTENT"
+            git push
+            return 0
+        else
+            echo # echo out a new line for better readability
+            rejected_commit_prompt="You generated this commit message for me before and I did not like it: $CONTENT."
+            push "$input. $rejected_commit_prompt"
+            return $?
+        fi
+    done
 }
